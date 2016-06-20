55bf8b67210b1abec108cbeef102628bc209a97f
copygof <precha.sattabut@gmail.com>
2016-06-20 18:27:26 +0700

    add tab menu

--- www/js/app.js
+++ www/js/app.js
@@ -1,68 +1,69 @@
-angular.module('starter', ['ionic'])
+angular.module('shop', ['ionic'])
 
     .config(function($stateProvider, $urlRouterProvider) {
 
-      $stateProvider
+      /*$stateProvider
           .state('tabs', {
             url: "/tab",
             abstract: true,
             templateUrl: "templates/tabs.html"
-          })
-          .state('tabs.home', {
-            url: "/home",
-            views: {
-              'home-tab': {
-                templateUrl: "templates/home.html",
-                controller: 'HomeTabCtrl'
-              }
-            }
-          })
-          .state('tabs.facts', {
-            url: "/facts",
-            views: {
-              'home-tab': {
-                templateUrl: "templates/facts.html"
-              }
-            }
-          })
-          .state('tabs.facts2', {
-            url: "/facts2",
-            views: {
-              'home-tab': {
-                templateUrl: "templates/facts2.html"
-              }
-            }
-          })
-          .state('tabs.about', {
-            url: "/about",
-            views: {
-              'about-tab': {
-                templateUrl: "templates/about.html"
-              }
-            }
-          })
-          .state('tabs.navstack', {
-            url: "/navstack",
-            views: {
-              'about-tab': {
-                templateUrl: "templates/nav-stack.html"
-              }
-            }
-          })
-          .state('tabs.contact', {
-            url: "/contact",
-            views: {
-              'contact-tab': {
-                templateUrl: "templates/contact.html"
-              }
-            }
+
           });
-
-
-      $urlRouterProvider.otherwise("/tab/home");
+      $urlRouterProvider.otherwise("/tab/home");*/
 
     })
 
-    .controller('HomeTabCtrl', function($scope) {
-      console.log('HomeTabCtrl');
+    .controller('viewShop', function($scope) {
+
+
+        var isList = true; // if have list product
+        if(isList){
+            $scope.content_list = false;
+        }else{
+            $scope.content_list = true;
+        }
+
+        $scope.content_tab_1 = false;
+        $scope.content_tab_2 = true;
+        $scope.content_tab_3 = true;
+
+
+        $scope.btnTabActive_1 = "active";
+        $scope.btnTabActive_2 = "";
+        $scope.btnTabActive_3 = "";
+
+        /*var addActive = angular.element( document.querySelector('.tab-item') );
+         addActive.addClass('active');*/
+
+
+          $scope.tabsClick = function (idTabs) {
+              console.log(idTabs)
+              if(idTabs == 1){
+                  $scope.content_tab_1 = false;
+                  $scope.content_tab_2 = true;
+                  $scope.content_tab_3 = true;
+
+                  $scope.btnTabActive_1 = "active";
+                  $scope.btnTabActive_2 = "";
+                  $scope.btnTabActive_3 = "";
+
+              }else if(idTabs == 2){
+                  $scope.content_tab_1 = true;
+                  $scope.content_tab_2 = false;
+                  $scope.content_tab_3 = true;
+
+                  $scope.btnTabActive_1 = "active";
+                  $scope.btnTabActive_2 = "";
+                  $scope.btnTabActive_3 = "";
+
+              }else if(idTabs == 3){
+                  $scope.content_tab_1 = true;
+                  $scope.content_tab_2 = true;
+                  $scope.content_tab_3 = false;
+
+                  $scope.btnTabActive_1 = "active";
+                  $scope.btnTabActive_2 = "";
+                  $scope.btnTabActive_3 = "";
+              }
+          }
     });
